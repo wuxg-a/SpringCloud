0584ac5ddb6e38debc85bf004efb29de227f98a5
wuxg-a <331437792@qq.com>
2018-10-26 18:19:31 +0800

    init spring cloud

---
+++ src/main/java/com/glodon/Application.java
@@ -0,0 +1,16 @@
+package com.glodon;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by wuxg-a on 2018/10/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
